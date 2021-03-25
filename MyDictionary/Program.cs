a3f23cece859cd175e32c86a617d74bdd0ff665d
Emre Altınay <81089561+Altinayemre@users.noreply.github.com>
2021-03-25 20:33:46 +0300

    Add files via upload

---
+++ MyDictionary/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace MyDictionary
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            MyDictionary<int, string> Citys = new MyDictionary<int, string>();
+            Citys.Add(07, "Antalya");
+            Citys.Add(15,"Burdur");
+            Citys.Add(34,"İstanbul");
+            Citys.Add(06,"Ankara");
+            Citys.Add(35, "İzmir");
+
+
+            Citys.List();
+            Console.WriteLine();
+            Console.WriteLine("Toplam Şehir Sayısı: "+Citys.Count); 
+            
+            Console.ReadLine();
+        }
+    }
+}
